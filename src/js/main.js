a797403064f106b1597ddd45618169a8883be812
David Kocman <david.kocman@icloud.com>
2019-10-08 00:47:46 +0200

    review, fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,40 @@
-var icons = document.querySelectorAll('.icon');
-var grahics = document.querySelectorAll('.graphic');
-var navToggle = document.querySelectorAll('.nav-toggle');
-var mobileNodes = document.querySelectorAll('.mobile-nav__items ul li a');
+var icons = document.querySelectorAll('.icon'),
+	grahics = document.querySelectorAll('.graphic'),
+	certs = document.querySelectorAll('.cert'),
+	navToggle = document.querySelectorAll('.nav-toggle'),
+	mobileNodes = document.querySelectorAll('.mobile-nav__items ul li a'),
+	ids = document.querySelectorAll('a[href^="#"]');
 
 /*
 //	Adds event listener to nodeList
 */
 function addEventListenerList(list, event, fn) {
 	for (var i = 0, len = list.length; i < len; i++) {
-		list[i].addEventListener(event, fn, false);
+		list[i].addEventListener(event, fn, true);
 	}
+}
+
+/*
+//	Adds active class after click on nav item
+*/
+function addActiveClass(nodeList, target) {
+	for (var i = 0, len = nodeList.length; i < len; i++) {
+		if (target == nodeList[i]) {
+			nodeList[i].classList.add('active');
+		} else {
+			nodeList[i].classList.remove('active');
+		}
+	}
+}
+
+/*
+//	Scrolls to nav item corresponding element
+*/
+function scrollToHash(hash) {
+	document.getElementById(hash).scrollIntoView({
+		behavior: 'smooth',
+		block: 'center'
+	});
 }
 
 window.sr = ScrollReveal();
@@ -22,6 +47,12 @@ sr.reveal(icons, {
 sr.reveal(grahics, {
 	delay: 100,
 	duration: 1500
+});
+sr.reveal(certs, {
+	delay: 500,
+	duration: 1500,
+	interval: 200,
+	distance: '40px'
 });
 
 var mySwiper = new Swiper('.swiper-container', {
@@ -64,4 +95,11 @@ addEventListenerList(navToggle, 'click', function (e) {
 addEventListenerList(mobileNodes, 'click', function (e) {
 	e.preventDefault();
 	document.body.classList.remove('menu-visible');
+});
+
+addEventListenerList(ids, 'click', function (e) {
+	var hash = e.target.hash.substr(1);
+	e.preventDefault();
+	addActiveClass(ids, e.target);
+	scrollToHash(hash, e.target);
 });
